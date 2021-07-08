048af3f913afcbc9902130b7379b7a124a8c839c
Guled Ahmed <akindnesstoremember@gmail.com>
2021-07-08 15:49:10 -0700

    changed the title of the main header located in the Hero.js file

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -61,18 +61,18 @@ const Hero = ({
         <div className={innerClasses}>
           <div className="hero-content">
             <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-              Landing template for <span className="text-color-primary">startups</span>
+              Somali Youth Mentorship <span className="text-color-warning">Help Is Here!</span>
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
-                Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.
+                We are dedicated in providing the right mentorship to our youth. Be the beacon of hope for our youth!
                 </p>
               <div className="reveal-from-bottom" data-reveal-delay="600">
                 <ButtonGroup>
-                  <Button tag="a" color="primary" wideMobile href="https://cruip.com/">
+                  <Button tag="a" color="primary" wideMobile href="#">
                     Get started
                     </Button>
-                  <Button tag="a" color="dark" wideMobile href="https://github.com/cruip/open-react-template/">
+                  <Button tag="a" color="dark" wideMobile href="https://github.com/Guled06/landing_page">
                     View on Github
                     </Button>
                 </ButtonGroup>
